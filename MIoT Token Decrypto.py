98eebbc75f4cb75bc7086d66cb85300531d28788
JNturtle <31470045+JNturtle@users.noreply.github.com>
2021-04-13 17:48:24 +0800

    Update MIoT Token Decrypto.py

--- MIoT Token Decrypto.py
+++ MIoT Token Decrypto.py
@@ -8,11 +8,12 @@ Key是固定的，為長度32的0，hex格式。
 """
 from base64 import b64decode
 from Crypto.Cipher import AES
-token_Sample = "d10d989a602f43d6ec0f23f373571cf71d9382570aa2a4aee48951c0b12baacb0143db63ee66b0cdff9f69917680151e"
+
 token = ""
 while len(token) != 96:
-    token = input("輸入長度為96的米家設備token：\n")
+    token = input("Encrypted MIoT token：\n")
+    if len(token) != 96:
+      print("Please Input a token, which length is 96.")
 MiKey = "00000000000000000000000000000000"
 result = AES.new(bytes.fromhex(MiKey), AES.MODE_ECB).decrypt(bytes.fromhex(token))[:32].decode("utf-8")
-print("解密後的token:\n{:}".format(result))
-
+print("Decrpyted token:\n{:}".format(result))
